311e4de286f73271b6cfd77552ffd31cbd158516
Gryff <gryffskin@gmail.com>
2016-06-24 16:00:28 +0100

    Tests for Dice, without mocking Random as I feel it's a very simple class

--- C#/YahtzeeKata/YahtzeeKata/Src/Dice.cs
+++ C#/YahtzeeKata/YahtzeeKata/Src/Dice.cs
@@ -4,14 +4,13 @@ namespace YahtzeeKata
 {
     public class Dice
     {
+        private int _value;
+
         public virtual void Roll()
         {
-            throw new NotImplementedException();
+            _value = new Random(DateTime.Now.Millisecond).Next(1, 7);
         }
 
-        public virtual int Value()
-        {
-            throw new NotImplementedException();
-        }
+        public virtual int Value() => _value;
     }
 }
---
+++ C#/YahtzeeKata/YahtzeeKata/Tests/DiceShould.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace YahtzeeKata.Tests
+{
+    [TestFixture]
+    public class DiceShould
+    {
+        [Test]
+        public void RollARandomNumber()
+        {
+            var dice = new Dice();
+
+            for (int i = 0; i < 10; i++)
+            {
+                dice.Roll();
+                Assert.That(dice.Value(), Is.InRange(1, 6));
+            }
+        }
+    }
+}
