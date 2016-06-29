e97af52aa7fbf9c99b955a5044cebe18802f30d1
Gryff <gryffskin@gmail.com>
2016-06-29 11:50:02 +0100

    Extract class Turn

--- C#/YahtzeeKata/YahtzeeKata/Tests/CategoryShould.cs
+++ C#/YahtzeeKata/YahtzeeKata/Tests/CategoryShould.cs
@@ -9,16 +9,15 @@ namespace YahtzeeKata.Tests
         private IConsole _console;
         private Category _category;
         private string _title;
-        private Dice _dice;
+        private Turn _turn;
 
         [SetUp]
         public void SetUp()
         {
             _console = Substitute.For<IConsole>();
             _title = "Ones";
-            _dice = Substitute.For<Dice>();
-
-            _category = new Category(_title, _console, _dice);
+            _turn = Substitute.For<Turn>();
+            _category = new Category(_title, _console, _turn);
         }
 
         [Test]
@@ -27,24 +26,6 @@ namespace YahtzeeKata.Tests
             _category.Play();
 
             _console.Received().PrintLine($"Category: {_title}");
-        }
-
-        [Test]
-        public void RollTheDice()
-        {
-            _category.Play();
-            
-            _dice.Received().RollDice();
-        }
-
-        [Test]
-        public void PrintTheDice()
-        {
-            _dice.DiceValues().Returns("D1:5 D2:5 D3:5 D4:5 D5:5");
-
-            _category.Play();
-
-            _console.Received().PrintLine("Dice: D1:5 D2:5 D3:5 D4:5 D5:5");
         }
     }
 }
---
+++ C#/YahtzeeKata/YahtzeeKata/Tests/TurnShould.cs
@@ -0,0 +1,39 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace YahtzeeKata.Tests
+{
+    [TestFixture]
+    public class TurnShould
+    {
+        private Turn _turn;
+        private Dice _dice;
+        private IConsole _console;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dice = Substitute.For<Dice>();
+            _console = Substitute.For<IConsole>();
+            _turn = new Turn(_dice, _console);
+        }
+
+        [Test]
+        public void RollTheDice()
+        {
+            _turn.PlayTurn();
+
+            _dice.Received().RollDice();
+        }
+
+        [Test]
+        public void PrintTheDice()
+        {
+            _dice.DiceValues().Returns("D1:5 D2:5 D3:5 D4:5 D5:5");
+
+            _turn.PlayTurn();
+
+            _console.Received().PrintLine("Dice: D1:5 D2:5 D3:5 D4:5 D5:5");
+        }
+    }
+}
