6a87607bb06723829fa5bd6b1a8300d08ef168cf
Gryff <gryffskin@gmail.com>
2016-07-07 16:48:19 +0100

    Expand acceptance test to force use of scoring rules

--- C#/YahtzeeKata/YahtzeeKata/Tests/YahtzeeFeature.cs
+++ C#/YahtzeeKata/YahtzeeKata/Tests/YahtzeeFeature.cs
@@ -23,13 +23,22 @@ namespace YahtzeeKata.Tests
 
             _numberGenerator = Substitute.For<Random>();
             _numberGenerator.Next(1, 7).Returns(
+                // first category
                 1, 2, 3, 4, 5, 
                 1, 1, 2, 3, 
-                1, 2);
+                1, 2,
+                // second category
+                2, 2, 1, 3, 2,
+                4, 4,
+                2, 1);
 
             _console.ReadLine().Returns(
+                // first category
                 "D2 D3 D4 D5",
-                "D4 D5");
+                "D4 D5",
+                // second category
+                "D3 D4",
+                "D3 D4");
 
             _dice = new Dice(5, _numberGenerator);
             _turn = new Turn(_dice, _console);
@@ -61,6 +70,13 @@ namespace YahtzeeKata.Tests
                 _console.PrintLine("Category Ones score: 4");
 
                 _console.PrintLine("Category: Twos");
+                _console.PrintLine("Dice: D1:2 D2:2 D3:1 D4:3 D5:2");
+                _console.PrintLine("[1] Dice to re-run:");
+                _console.PrintLine("Dice: D1:2 D2:2 D3:4 D4:4 D5:2");
+                _console.PrintLine("[2] Dice to re-run:");
+                _console.PrintLine("Dice: D1:2 D2:2 D3:2 D4:1 D5:2");
+                _console.PrintLine("Category Twos score: 4");
+
                 _console.PrintLine("Category: Threes");
             });
         }
